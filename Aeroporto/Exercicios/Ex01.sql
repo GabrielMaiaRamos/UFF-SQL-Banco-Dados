1c540f31ed261adabeb2f37672f520b3f288b7b6
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-15 16:18:18 -0300

    ex01 e ex02

---
+++ Aeroporto/Exercicios/Ex01.sql
@@ -0,0 +1,4 @@
+--Faça uma consulta que retorne todos os aeroportos da cidade de London. 
+SELECT *
+FROM airport
+WHERE CityName = 'London'
---
+++ Aeroporto/Exercicios/Ex02.sql
@@ -0,0 +1,4 @@
+-- Faça uma consulta que retorne os aeroportos que possuem mais de 1 terminal.
+SELECT *
+FROM airport
+WHERE NumTerminals > 1
