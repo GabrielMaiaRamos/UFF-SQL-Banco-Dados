3f783dd52a85d3dd036d7307e17f2bc020cb6e65
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-18 11:39:02 -0300

    ex09

---
+++ Films/Exercicios/ex09.sql
@@ -0,0 +1,4 @@
+--Faça uma consulta que retorne os nomes dos filmes que possuem algum ator que fez o papel de "Narrator".
+SELECT m.name
+FROM movies m, roles r
+WHERE m.movie_id = r.movie_id AND r.role = 'Narrator';
