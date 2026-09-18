8516f58edd3f693f612b63d43e3f93597e7cea0c
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-18 12:35:45 -0300

    fix ex05

--- Films/Exercicios/ex05.sql
+++ Films/Exercicios/ex05.sql
@@ -1,7 +1,7 @@
 --Faça uma consulta que retorna os nomes dos filmes e o rank dos filmes que possuem rank maior do que 8, juntamente com o primeiro nome e sobrenome de seus diretores, ordenado pelo rank.
 SELECT m.name, m.rank, d.first_name, d.last_name
 FROM movies m, directors d, movies_directors md
-WHERE rank > 8
+WHERE m.rank > 8
 	AND md.director_id = d.director_id
     AND md.movie_id = m.movie_id
-ORDER BY m.rank
+ORDER BY m.rank;
