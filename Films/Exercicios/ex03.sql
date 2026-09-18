adf47b4a65059f494f5a2ed1d0104c4b6bab83f9
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-18 12:33:25 -0300

    ex03

---
+++ Films/Exercicios/ex03.sql
@@ -0,0 +1,7 @@
+--Faça uma consulta que retorna os nomes dos filmes lançados após o ano de 2000 (year), o primeiro nome e sobrenome dos atores que atuaram nesses filmes, juntamente com seus papéis, ordenado pelo nome do filme.
+SELECT m.name, a.first_name, a.last_name, r.role
+FROM movies m, actors a, roles r
+WHERE m.year > 2000
+	AND r.movie_id = m.movie_id
+    AND r.actor_id = a.actor_id
+ORDER BY m.name
