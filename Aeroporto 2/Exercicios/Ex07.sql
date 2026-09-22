b0b74735a66725a66165eac6b0a629a23dba35da
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-22 16:10:25 -0300

    fix ex07

--- Aeroporto 2/Exercicios/Ex07.sql
+++ Aeroporto 2/Exercicios/Ex07.sql
@@ -1,6 +1,6 @@
 -- 7. Faça uma consulta que retorna o nome do passageiro, a data do voo (FlightDate), e o AirportID da Origem e Destino dos voos de cada passageiro. Os nomes das colunas do ID do aeroporto devem ser Origem e Destino, respectivamente.
-SELECT p.paxname, p.FlightDate, r.Origin, r.Destination
+SELECT p.paxname, p.FlightDate, r.Origin AS Origem, r.Destination AS Destino
 FROM pax p, flight f, route r
 WHERE
 	p.FlightID = f.FlightID AND
-    f.RouteID = r.RouteID
+	f.RouteID = r.RouteID;
