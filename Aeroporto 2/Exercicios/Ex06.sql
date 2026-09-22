bbf41a0afb2af94fdd1f10dc794887a061531f5f
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-22 15:37:58 -0300

    ex06

---
+++ Aeroporto 2/Exercicios/Ex06.sql
@@ -0,0 +1,6 @@
+-- 6. Faça uma consulta que retorne todos os voos (número do voo e nome da aeronave) que operam num avião da Airbus (tabela aircrafttype).
+SELECT f.FlightID, at.AircraftName
+FROM flight f, aircraft a, aircrafttype at
+WHERE at.AircraftName LIKE 'Airbus%' AND
+	f.AircraftID = a.AircraftID AND
+    a.AircraftTypeID = at.AircraftTypeID
