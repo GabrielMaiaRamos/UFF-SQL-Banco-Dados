96491f1edbe082396ee750a913c890c922c5d699
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-22 16:12:31 -0300

    fix ex09

--- Aeroporto 2/Exercicios/Ex09.sql
+++ Aeroporto 2/Exercicios/Ex09.sql
@@ -1,7 +1,7 @@
 -- 9. Faça uma consulta que retorna o número do voo, o código do aeroporto de destino do voo (AirportCode) e a quantidade máxima de assentos (MaxSeats) da classe 3 (ClassID), ordenado de forma decrescente pela quantidade máxima de assentos.
 SELECT f.FlightID, a.AirportCode, fc.MaxSeats
 FROM flight f, route r, airport a, class c, flightclass fc
-WHERE c.ClassID = '3' AND
+WHERE c.ClassID = 3 AND
 	r.RouteID = f.RouteID AND
 	fc.FlightID = f.FlightID AND
     fc.ClassID = c.ClassID AND
