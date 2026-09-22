51b414e1ab722dcc805e71e02aefc0c232b64552
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-22 16:08:41 -0300

    fix ex02

--- Aeroporto 2/Exercicios/Ex02.sql
+++ Aeroporto 2/Exercicios/Ex02.sql
@@ -1,5 +1,5 @@
 -- 2. Faça uma consulta que retorna o código e nome dos aeroportos da Espanha (countryCode = 'ES'), ordenado pelo código do aeroporto.
-SELECT a.AirportID, a.AirportName
+SELECT a.AirportCode, a.AirportName
 FROM airport a
 WHERE a.CountryCode = 'ES'
-ORDER BY a.AirportID
+ORDER BY a.AirportCode
