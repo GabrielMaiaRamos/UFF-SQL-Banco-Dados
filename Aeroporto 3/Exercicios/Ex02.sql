3053da52281ea7732e11c1d834bf7e5f65f31116
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-26 11:56:15 -0300

    fix

--- Aeroporto 3/Exercicios/Ex02.sql
+++ Aeroporto 3/Exercicios/Ex02.sql
@@ -1,8 +1,8 @@
 -- 2. Repita a consulta anterior, agora usando UNION para produzir a resposta.
 SELECT r.RouteID, r.Origin, r.Destination
-FROM route r JOIN airport a ON r.Origin = a.AirportID
+FROM route r INNER JOIN airport a ON r.Origin = a.AirportID
 WHERE a.AirportCode = 'LHR'
 UNION
 SELECT r.RouteID, r.Origin, r.Destination
-FROM route r JOIN airport a ON r.Origin = a.AirportID
+FROM route r INNER JOIN airport a ON r.Origin = a.AirportID
 WHERE a.AirportCode = 'AMS';
