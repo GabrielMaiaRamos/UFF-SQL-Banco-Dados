352838c9c70851ba63e017cbece0af605f464787
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-26 11:41:25 -0300

    ex01

---
+++ Aeroporto 3/Exercicios/Ex01.sql
@@ -0,0 +1,5 @@
+-- 1. Faça uma consulta que retorna as rotas (RouteID, Origin, Destination) que possuem como origem o aeroporto LHR ou o aeroporto AMS. Use o comando de JOIN apropriado.
+SELECT r.RouteID, r.Origin, r.Destination
+FROM route r JOIN airport a ON r.Origin = a.AirportID
+WHERE a.AirportCode = 'LHR' OR
+	a.AirportCode = 'AMS'
