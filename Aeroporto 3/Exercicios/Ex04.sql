5825079aae3a97368f79492f1689bccc1dab050c
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-09-26 12:18:54 -0300

    ex04

---
+++ Aeroporto 3/Exercicios/Ex04.sql
@@ -0,0 +1,3 @@
+-- 4. Faça uma consulta que retorne todas as informações de vôo (flight) e os horários de partida desse vôo (tabela flightdep). Use o operador de junção apropriado. O resultado deve estar ordenado por FlightID.
+SELECT f.*, fd.DepTime
+FROM flight f INNER JOIN flightdep fd ON f.FlightID = fd.FlightID
